d9dadeca0215039f721ceb7c7c1aa86d3bbb3f5a
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2025-10-07 17:08:24 +0300

    ci: update `make mini_nvim` to always update cleanly

--- _scripts/mini_nvim.sh
+++ _scripts/mini_nvim.sh
@@ -1,16 +1,18 @@
-# Copy necessary files with proper routing
-if [[ ! -d "mini.nvim" ]]
+# Clean copy necessary files with proper routing
+if [[ -d "mini.nvim" ]]
 then
-  mkdir -p mini.nvim
-  cp _deps/mini.nvim/README.md mini.nvim/index.md
-  cp _deps/mini.nvim/logo.png mini.nvim/logo.png
-  cp _deps/mini.nvim/CHANGELOG.md mini.nvim/CHANGELOG.md
-  cp _deps/mini.nvim/CONTRIBUTING.md mini.nvim/CONTRIBUTING.md
-  cp _deps/mini.nvim/TESTING.md mini.nvim/TESTING.md
-  mkdir -p mini.nvim/scripts
-  cp _deps/mini.nvim/scripts/init-deps-example.lua mini.nvim/scripts/init-deps-example.lua
-  cp -r _deps/mini.nvim/readmes mini.nvim/readmes
-  cp -r _deps/mini.nvim/doc mini.nvim/doc
+  rm -rf mini.nvim
 fi
+
+mkdir -p mini.nvim
+cp _deps/mini.nvim/README.md mini.nvim/index.md
+cp _deps/mini.nvim/logo.png mini.nvim/logo.png
+cp _deps/mini.nvim/CHANGELOG.md mini.nvim/CHANGELOG.md
+cp _deps/mini.nvim/CONTRIBUTING.md mini.nvim/CONTRIBUTING.md
+cp _deps/mini.nvim/TESTING.md mini.nvim/TESTING.md
+mkdir -p mini.nvim/scripts
+cp _deps/mini.nvim/scripts/init-deps-example.lua mini.nvim/scripts/init-deps-example.lua
+cp -r _deps/mini.nvim/readmes mini.nvim/readmes
+cp -r _deps/mini.nvim/doc mini.nvim/doc
 
 nvim -l ./_scripts/mini_nvim.lua
